35ef4281725b4a1fea3fe7a5f67f4a7a704099d4
wengjun <2328952185@qq.com>
2020-12-08 20:51:35 +0800

    finish the homework.

--- MinuitFit2.C
+++ MinuitFit2.C
@@ -1,11 +1,11 @@
-const int npoints=5000;
+const int npoints=50000;
 Double_t x[npoints];
 
 int get_input_data();
 void fcn(Int_t &npar, Double_t *gin, Double_t &chi2, Double_t *par, Int_t iflag);
 void minuit();
 
-void MinuitFit()
+void MinuitFit2()
 {
   // Get data points
    get_input_data();
@@ -28,12 +28,12 @@ void minuit() {
    Double_t step[npar] = { 0.01 ,0.01};
    gMinuit->mnparm(0, "beta", 0.9, 0.01, 0,0,ierflg);
    gMinuit->mnparm(1,"alpha",0.9,0.01,0,0,ierflg);
-//   gMinuit->mnparm(2,"Ntot",1000,1,0,0,ierflg);
+//   gMinuit->mnparm(2,"Ntot",10000,1000,0,0,ierflg);
 
 // Now ready for minimization step
-   arglist[0] = 0.9;
-   arglist[1] = 0.9;
-//   arglist[2]=1000;
+   arglist[0] = 1;
+   arglist[1] = 1;
+//   arglist[2] = 1;
 
    gMinuit->mnexcm("MIGRAD", arglist ,0,ierflg);
    gMinuit->mnexcm("HESSE", arglist ,0,ierflg);
@@ -58,9 +58,9 @@ void fcn(Int_t &npar, Double_t *gin, Double_t &chi2,
    Double_t log_l = 0;
    Double_t beta = par[0];
    Double_t alpha=par[1];
-//   Double_t Ntot=par[2];
+   Double_t Ntot=par[2];
    for (Int_t i=0;i<npoints; i++) {
-     log_l += log(3)-log(2*(3+beta))+log(1+alpha*x[i]+beta*x[i]*x[i]/*+log(Ntot)*/);
+     log_l += log(3)-log(5.7+2*beta*0.95*0.95*0.95)+log(1+alpha*x[i]+beta*x[i]*x[i])/*+log(Ntot))*/;
    }
    // factor 2 to get errors right
    chi2 = -2.0*log_l;  
@@ -70,9 +70,9 @@ int get_input_data()
 {
    // Generate data points according to the user defined pdf
    Double_t betaTrue = 1.0;
-   TH1F *h1 = new TH1F("h1","x",100,-1,1);
+   TH1F *h1 = new TH1F("h1","x",100,-0.95,0.95);
    double sum=0.0;
-   TF1 *fnc=new TF1("fnc","[n]*3/(2*(3+[beta]))*(1+[alpha]*x+[beta]*x*x)",-1,1);
+   TF1 *fnc=new TF1("fnc","[n]*3/(2*(3+[beta]))*(1+[alpha]*x+[beta]*x*x)",-0.95,0.95);
     float p1=1;
     float p2=1;
         fnc->SetParameters(1,p1,p2);
@@ -81,7 +81,7 @@ int get_input_data()
         int i=1;
         while(i<=npoints){
 		gRandom->SetSeed();
-                float a=gRandom->Uniform(-1,1);
+                float a=gRandom->Uniform(-0.95,0.95);
                 float b=gRandom->Uniform(0,maxy);
                 float c=3/(2*(3+p1))*(1+p2*a+p1*a*a);
                 if(b<=c){
--- homeex.C
+++ homeex.C
@@ -1,17 +1,16 @@
 void homeex(){
-	TF1 *fnc=new TF1("fnc","3/(2*(3+[beta]))*(1+[alpha]*x+[beta]*x*x)",-1,1);
+	TF1 *fnc=new TF1("fnc","[n]*3/(2*(3+[beta]))*(1+[alpha]*x+[beta]*x*x)",-0.95,0.95);
 	float p1=1;
 	float p2=1;
 	float x[5000];
-	fnc->SetParameters(p1,p2);
+	fnc->SetParameters(1,p1,p2);
 	float maxy=fnc->GetMaximum();
 	float miny=fnc->GetMinimum();
-	TH1F *hist=new TH1F("hist","hist",60,-1,1);
+	TH1F *hist=new TH1F("hist","hist",60,-0.95,0.95);
 	int i=1;
 	while(i<=5000){
-		float a=gRandom->Uniform(-1,1);
+		float a=gRandom->Uniform(-0.95,0.95);
 		float b=gRandom->Uniform(0,maxy);
-		hist0->Fill(b);
 		float c=3/(2*(3+p1))*(1+p2*a+p1*a*a);
 		if(b<=c){
 			hist->Fill(a);
@@ -20,7 +19,11 @@ void homeex(){
 		}
 	}
 	TCanvas *c1=new TCanvas("c1","");
-	hist->Draw();
+//	fnc->Draw();
+//	hist->Draw();
+	hist->Fit(fnc,"l");
+//	TCanvas *c2=new TCanvas("c2","");
+//	hist->Draw();
 }
 
 
